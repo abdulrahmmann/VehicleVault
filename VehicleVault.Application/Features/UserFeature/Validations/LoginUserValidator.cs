cac7066816f7a05c11c62dd03a71f41bddeae92a
Abdulrahman <abd3lra7man.078@gmail.com>
2025-08-01 18:15:04 +0300

    Create LoginUserValidator.

---
+++ VehicleVault.Application/Features/UserFeature/Validations/LoginUserValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using VehicleVault.Application.Features.UserFeature.DTOs;
+
+namespace VehicleVault.Application.Features.UserFeature.Validations;
+
+public class LoginUserValidator: AbstractValidator<UserRegisterDto>
+{
+    public LoginUserValidator()
+    {
+        RuleFor(user => user.Email)
+            .EmailAddress().WithMessage("Invalid email format")
+            .NotEmpty().WithMessage("Email must not be empty.")
+            .NotNull().WithMessage("Email must not be null");
+        
+        RuleFor(s => s.Password)
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .MaximumLength(100).WithMessage("Password cannot exceed 100 characters.")
+            .NotEmpty().WithMessage("Password must not be empty.")
+            .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$")
+            .WithMessage("Password must contain uppercase, lowercase, number, and special character.");
+    }
+}
